ae83eca95179055038ad4c799018693918c5562e
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-06-27 18:10:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-python-ec2-app',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[],  # Add dependencies here if needed
     entry_points={

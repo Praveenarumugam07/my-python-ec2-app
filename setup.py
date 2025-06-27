0bc33c25a48895173582e6c0e4d533c9e31af305
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-06-27 16:00:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-python-ec2-app',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here if needed
+    entry_points={
+        'console_scripts': [
+            'myapp = my_app.main:main'
+        ]
+    }
+)
